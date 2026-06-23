74cc2a07ef75fb456f8e99ece469f598d7b6d216
ri-ty <riyatyagi7120@gmail.com>
2026-06-23 23:45:36 +0530

    Add basePath and unoptimized images settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/ek-koshish-ngo",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
